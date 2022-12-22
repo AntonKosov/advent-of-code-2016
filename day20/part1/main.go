049be5ff239e3b3aad72ac968eb4b9da3e520317
Anton Kosov <AntonRKosov@gmail.com>
2022-12-22 16:15:57 -0600

    day20

---
+++ day20/part1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/AntonKosov/advent-of-code-2016/aoc"
+)
+
+const (
+	minValue = 0
+	maxValue = 4294967295
+)
+
+func main() {
+	data := read()
+	r := process(data)
+	fmt.Printf("Answer: %v\n", r)
+}
+
+func read() []ipRange {
+	lines := aoc.ReadAllInput()
+	lines = lines[:len(lines)-1]
+
+	ipRanges := make([]ipRange, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, "-")
+		ipRanges[i] = ipRange{from: aoc.StrToInt(parts[0]), to: aoc.StrToInt(parts[1])}
+	}
+
+	return ipRanges
+}
+
+func process(ipRanges []ipRange) int {
+	sort.Slice(ipRanges, func(i, j int) bool { return ipRanges[i].from < ipRanges[j].from })
+	prevMax := minValue - 1
+	for _, r := range ipRanges {
+		if prevMax+1 < r.from {
+			return prevMax + 1
+		}
+		prevMax = aoc.Max(prevMax, r.to)
+	}
+
+	panic("no allowed addresses")
+}
+
+type ipRange struct {
+	from, to int
+}
